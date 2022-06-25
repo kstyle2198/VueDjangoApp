03e98c3f3712a134a309a6c1596b3f4cced70f96
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-06-25 23:15:37 +0900

    add taggit

--- backend/a_mysite/settings.py
+++ backend/a_mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+
     'b_blog.apps.BBlogConfig',
 ]
 
@@ -124,3 +126,5 @@ STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAGGIT_CASE_INSENSITIVE = True
--- backend/b_blog/admin.py
+++ backend/b_blog/admin.py
@@ -4,4 +4,10 @@ from b_blog.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', 'create_dt', 'update_dt')
+    list_display = ('id', 'title', 'description', 'create_dt', 'update_dt', 'tag_list')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())

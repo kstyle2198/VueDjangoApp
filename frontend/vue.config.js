7857395edcd2bbc49693adb25d1d481975950d61
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-06-26 08:52:36 +0900

    frontend + add dist/static folder

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  outputDir: 'dist',
+  publicPath: '/',
+  assetsDir: 'static',
 }
+
+// 상세 설명 찾아보기 https://cli.vuejs.org/config/#publicpath

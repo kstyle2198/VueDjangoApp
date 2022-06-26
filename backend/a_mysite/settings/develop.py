3231b09a5bbaae203e2fabd9cf313d2e4754a93c
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-06-26 11:43:59 +0900

    correct staticfiles_dir in settings.py

--- backend/a_mysite/settings/develop.py
+++ backend/a_mysite/settings/develop.py
@@ -10,6 +10,6 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }

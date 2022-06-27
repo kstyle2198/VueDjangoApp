fc8d71c588f13686c67c9d6fd6948d81fb387f65
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-06-27 22:08:51 +0900

    add login form

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -19,13 +19,13 @@
       <v-toolbar-title>VueDjangoAPP</v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <v-btn text href="/"> Home </v-btn>
-      <v-btn text href="/blog/post/list/"> Blog </v-btn>
-      <v-btn text href="/admin/"> Admin </v-btn>
-      <v-btn text> / </v-btn>
+      <v-btn text href="/">Home</v-btn>
+      <v-btn text href="/blog/post/list/">Blog</v-btn>
+      <v-btn text href="/admin/">Admin</v-btn>
+      <v-btn text>/</v-btn>
 
-      <v-btn text href="post_list.html"> PostList </v-btn>
-      <v-btn text href="post_detail.html"> PostDetail </v-btn>
+      <v-btn text href="post_list.html">PostList</v-btn>
+      <v-btn text href="post_detail.html">PostDetail</v-btn>
       <v-spacer></v-spacer>
 
       <v-menu offset-y left bottom>
@@ -38,7 +38,7 @@
         </template>
 
         <v-list>
-          <v-list-item>
+          <v-list-item @click="dialog = true">
             <v-list-item-title>Login</v-list-item-title>
           </v-list-item>
           <v-list-item>
@@ -53,6 +53,36 @@
         </v-list>
       </v-menu>
     </v-app-bar>
+
+    <v-dialog v-model="dialog" max-width="450">
+      <v-card class="elevation-12">
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title>Login form</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Login"
+              name="login"
+              prepend-icon="mdi-account"
+              type="text"
+            ></v-text-field>
+            <v-text-field
+              id="password"
+              label="Password"
+              name="password"
+              prepend-icon="mdi-lock"
+              type="password"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="grey" text @click="dialog = false">Cancel</v-btn>
+          <v-btn color="primary" class="mr-5" @click="dialog = false">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -60,13 +90,14 @@
 export default {
   data: () => ({
     drawer: null,
+    dialog: false,
     items: [
       { title: 'Dashboard', icon: 'mdi-view-dashboard' },
       { title: 'Photos111', icon: 'mdi-image' },
       { title: 'About1111', icon: 'mdi-help-box' },
     ],
   }),
-};
+}
 </script>
 
 <style></style>
